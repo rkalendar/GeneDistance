ef68129879da3b6eeb80495043671569f330503b
Ruslan Kalendar <78969155+rkalendar@users.noreply.github.com>
2025-04-24 19:22:14 +0500

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
-java -jar C:\MyPrograms\Java\KmersPattern\dist\KmersPattern.jar C:\MyPrograms\Java\KmersPattern\test\b1.txt
+java -jar C:\KmersPattern\dist\KmersPattern.jar C:\KmersPattern\test\b1.txt
 
 
-java -jar C:\MyPrograms\Java\KmersPattern\dist\KmersPattern.jar E:\Genomes\Oryza_sativa\
+java -jar C:\KmersPattern\dist\KmersPattern.jar E:\Genomes\Oryza_sativa\
